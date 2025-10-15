f3c545c70bfc3b8dfab31ecaa93db90f139af759
reaksmeyun <reaksmeyun1@gmail.com>
2025-10-15 23:34:12 +0700

    Re-adding start.sh file with correct permissions

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# 1. Collects all static files (CSS/JS)
+python manage.py collectstatic --no-input
+
+# 2. Applies database migrations
+python manage.py migrate
+
+# 3. Starts the Gunicorn web server
+gunicorn sentiment_project.wsgi:application
