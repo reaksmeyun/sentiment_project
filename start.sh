8bd8c229af8bf1cca51dc753831e46956e156dd4
reaksmeyun <reaksmeyun1@gmail.com>
2025-10-17 21:47:50 +0700

    Ready for Render deployment

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# Collect static files
+
 python manage.py collectstatic --no-input
 
-# Apply database migrations
+
 python manage.py migrate
 
-# Start Gunicorn server
+
 gunicorn sentiment_project.wsgi:application --bind 0.0.0.0:$PORT
 
